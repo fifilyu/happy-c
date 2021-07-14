3c1ebc5f80d7c8390e8b88c5f048b77b73fa00d0
Fifi Lyu <fifilyu@gmail.com>
2021-07-14 15:26:10 +0800

    格式化代码

--- include/happyc/byte.h
+++ include/happyc/byte.h
@@ -5,13 +5,21 @@
 #ifndef HAPPY_C_BYTE_H
 #define HAPPY_C_BYTE_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include "happyc.h"
 #include <stdint.h>
 
-uint16_t from_2_bytes(const byte_t *bytes, int len, int *error_code);
-uint32_t from_4_bytes(const byte_t *bytes, int len, int *error_code);
+HAPPYC_SHARED_LIB_API uint16_t from_2_bytes(const byte_t *bytes, int len, int *error_code);
+HAPPYC_SHARED_LIB_API uint32_t from_4_bytes(const byte_t *bytes, int len, int *error_code);
 
-byte_t *to_2_bytes(uint16_t i);
-byte_t *to_4_bytes(uint32_t i);
+HAPPYC_SHARED_LIB_API byte_t *to_2_bytes(uint16_t i);
+HAPPYC_SHARED_LIB_API byte_t *to_4_bytes(uint32_t i);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif //HAPPY_C_BYTE_H
--- src/byte.c
+++ src/byte.c
@@ -3,10 +3,13 @@
 //
 
 #include "happyc/byte.h"
-#include <stdio.h>
 #include <stdlib.h>
 
-uint16_t from_2_bytes(const byte_t *bytes, int len, int *error_code) {
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+HAPPYC_SHARED_LIB_API uint16_t from_2_bytes(const byte_t *bytes, int len, int *error_code) {
     if (len != 2) {
         *error_code = -1;
         return 0;
@@ -16,7 +19,7 @@ uint16_t from_2_bytes(const byte_t *bytes, int len, int *error_code) {
     return ((bytes[0] & 0xFF) << 8) | ((bytes[1] & 0xFF));
 }
 
-uint32_t from_4_bytes(const byte_t *bytes, int len, int *error_code) {
+HAPPYC_SHARED_LIB_API uint32_t from_4_bytes(const byte_t *bytes, int len, int *error_code) {
     if (len != 4) {
         *error_code = -1;
         return 0;
@@ -26,7 +29,7 @@ uint32_t from_4_bytes(const byte_t *bytes, int len, int *error_code) {
     return ((bytes[0] & 0xFF) << 24) | ((bytes[1] & 0xFF) << 16) | ((bytes[2] & 0xFF) << 8) | ((bytes[3] & 0xFF));
 }
 
-byte_t *to_2_bytes(uint16_t i) {
+HAPPYC_SHARED_LIB_API byte_t *to_2_bytes(uint16_t i) {
     byte_t *bytes = malloc(sizeof(byte_t) * 2);
 
     bytes[0] = (i >> 8) & 0xFF;
@@ -35,7 +38,7 @@ byte_t *to_2_bytes(uint16_t i) {
     return bytes;
 }
 
-byte_t *to_4_bytes(uint32_t i) {
+HAPPYC_SHARED_LIB_API byte_t *to_4_bytes(uint32_t i) {
     byte_t *bytes = malloc(sizeof(byte_t) * 4);
 
     bytes[0] = (i >> 24) & 0xFF;
@@ -45,3 +48,7 @@ byte_t *to_4_bytes(uint32_t i) {
 
     return bytes;
 }
+
+#ifdef __cplusplus
+}
+#endif
